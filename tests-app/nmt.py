c9e54a16340558af06d0311295267478682f1e5a
ololoshka2871 <SweetTreasure@2ch.hk>
2019-08-27 13:45:08 +0300

    state problem.. testing

--- tests-app/nmt.py
+++ tests-app/nmt.py
@@ -35,6 +35,10 @@ class TestNMT(object):
     def _reset_delay(self):
         time.sleep(0.5)
 
+    def test_initial_state(self):
+        lss_set_node_id(self.network, self.node_id)
+        assert self.node.nmt.state == 'PRE-OPERATIONAL'
+
     def test_reset_com(self):
         lss_set_node_id(self.network, self.node_id)
         self.network.nmt.state = 'RESET COMMUNICATION'
@@ -131,6 +135,7 @@ class TestNMT(object):
 
         self.network = create_network()
         lss_set_node_id(self.network, self.node_id)
+        self._reset_delay()
         self.node.associate_network(self.network)
         assert self.node.nmt.state == 'PRE-OPERATIONAL'
 
@@ -145,6 +150,7 @@ class TestNMT(object):
 
         self.network = create_network()
         self.node.associate_network(self.network)
+        self._reset_delay()
         assert self.node.nmt.state == 'PRE-OPERATIONAL'
         assert self.node.sdo[0x1200][0].raw is not None
         self.network.disconnect()
