6dc695f9dc09412dfecf5cafef16ab7a90c91166
ololoshka2871 <SweetTreasure@2ch.hk>
2019-08-28 15:07:51 +0300

    Start testing SDO, read all

---
+++ tests-app/sdo_read_all.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from common import *
+import canopen
+import time
+
+
+class ODEntry:
+    def __init__(self, index, subindex=None):
+        self.index = index
+        self.subindex = subindex
+
+    @staticmethod
+    def createEntries(sdo, od_entrie):
+        if isinstance(sdo[od_entrie], canopen.sdo.base.Record):
+            return [str(ODEntry(od_entrie, sub)) for sub in sdo[od_entrie].keys()]
+        else:
+            return [str(ODEntry(od_entrie))]
+
+    def __str__(self):
+        return '{:X}sub{:X}'.format(self.index, self.subindex) if self.subindex is not None \
+            else '{:X}'.format(self.index)
+
+    @staticmethod
+    def parce(s):
+        v = s.split('sub')
+        return ODEntry(int(v[0], 16), int(v[1], 16) if len(v) == 2 else None)
+
+    def getvalue(self, sdo):
+        if self.subindex is None:
+            return sdo[self.index].raw
+        else:
+            return sdo[self.index][self.subindex].raw
+
+
+def pytest_generate_tests(metafunc):
+    node = canopen.RemoteNode(1, 'SCTB_CANopenPressureSensor0xC001.eds')
+    entries = []
+    for od_addr in node.sdo.keys():
+        entries.extend(ODEntry.createEntries(node.sdo, od_addr))
+    metafunc.parametrize('odEntry', entries)
+
+
+class TestSDOReadAll(object):
+    @classmethod
+    def setup_class(cls):
+        cls.network = create_network()
+        cls.node_id = 16
+        reset_network(cls.network)
+
+        # lss prepare node
+        lss_waiting_state(cls.network)
+        lss_set_node_id(cls.network, cls.node_id)
+        cls.node = canopen.RemoteNode(cls.node_id, 'SCTB_CANopenPressureSensor0xC001.eds')
+        cls.node.associate_network(cls.network)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.network.disconnect()
+
+    def test_read_SDO_entries(self, odEntry):
+        entry = ODEntry.parce(odEntry)
+        if isinstance(self.node.sdo[entry.index], canopen.sdo.base.Array):
+            for subindex in self.node.sdo[entry.index].keys():
+                try:
+                    v = self.node.sdo[entry.index][subindex].raw
+                except canopen.sdo.exceptions.SdoAbortedError as f:
+                    if entry.index == 0x1003 and subindex > 0 and f.code == 0x08000023:
+                        pass  # лог ошибок может быть пуст
+                    elif f.code != 0x06010001:
+                        raise f
+        else:
+            try:
+                v = entry.getvalue(self.node.sdo)
+            except canopen.sdo.exceptions.SdoAbortedError as f:
+                if f.code != 0x06010001:
+                    raise f
