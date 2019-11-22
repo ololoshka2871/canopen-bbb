dff96e50d7b5d5862a89a3f16f1aecddc5abd8d0
ololoshka2871 <SweetTreasure@2ch.hk>
2019-11-22 13:13:52 +0300

    Manual TPDO

---
+++ tests-app/TPDO_manual.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from common import *
+from ODEntry import ODEntry
+import argparse
+import canopen
+import time
+import datetime
+
+first_result = True
+
+
+def prepare_node(node_id, eds_file):
+    network = create_network()
+    node = canopen.RemoteNode(node_id, eds_file)
+    node.associate_network(network)
+    node.nmt.state = 'PRE-OPERATIONAL'
+    return node
+
+
+def close(node):
+    nw = node.network
+    node.remove_network()
+    nw.disconnect()
+
+
+def reset_com_if_nessucery(node):
+    error_flags = node.sdo[0x1001].raw
+    if error_flags != 0:
+        print('Node has errors: {}, resetting communication'.format(error_flags))
+        node.nmt.state = 'RESET COMMUNICATION'
+        node.nmt.wait_for_bootup()
+
+
+def create_table_header(msg):
+    columns = [res.name for res in msg]
+    print('Date;{}'.format(';'.join(columns)))
+
+
+def print_result(msg):
+    results = [res.raw for res in msg]
+    now = datetime.datetime.now()
+    nowfmt = now.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (now.microsecond / 10000))
+
+    print('{};{}'.format(nowfmt, ';'.join(map(str,results))))
+
+
+def tpdo_cb(msg):
+    global first_result
+    if first_result:
+        create_table_header(msg)
+        first_result = False
+    print_result(msg)
+
+
+def add_tpo_variable(tpdo, index):
+    entry = ODEntry.parce(index)
+    print('Mapping {}...'.format(entry))
+    if entry.subindex is None:
+        tpdo.add_variable(entry.index)
+    else:
+        tpdo.add_variable(entry.index, entry.subindex)
+
+
+def do_reading(node, sync_period):
+    print('Start reading...')
+    node.nmt.state = 'OPERATIONAL'
+    if sync_period > 0:
+        node.network.sync.start(sync_period)
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        pass
+    node.network.sync.stop()
+    node.nmt.state = 'PRE-OPERATIONAL'
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Can Open manual TPDO reader')
+    parser.add_argument('-N', '--nodeid', required=True, type=int, help='Node ID to connect to')
+    parser.add_argument('--eds', required=True, type=str, help='Device electronic datasheet')
+    parser.add_argument('-T', '--tpdo', required=True, type=int, help='TPDO number to use')
+    parser.add_argument('-t', '--transtype', default=254, type=int, help="TPDO transmission type")
+    parser.add_argument('--event_timer', type=int, default=100, help="TPO event timer value, ms")
+    parser.add_argument('-S', '--sync', type=float, default=0, help='SYNC period')
+    parser.add_argument('index', type=str, nargs='+', help='Indexes of mapped objects')
+    args = parser.parse_args()
+
+    node = prepare_node(args.nodeid, args.eds)
+
+    reset_com_if_nessucery(node)
+
+    assert args.tpdo > 0
+
+    if 1 <= args.transtype <= 0xF0:
+        assert args.sync > 0
+
+    node.tpdo.read()
+    tpdo = node.tpdo[args.tpdo]
+    tpdo.clear()
+
+    for index in args.index:
+        add_tpo_variable(tpdo, index)
+
+    tpdo.trans_type = args.transtype
+    tpdo.event_timer = args.event_timer
+    tpdo.enabled = True
+
+    node.tpdo.save()
+    tpdo.add_callback(tpdo_cb)
+
+    do_reading(node, args.sync)
+
+    tpdo.clear()
+    close(node)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests-app/tpdo.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from common import *
+from ODEntry import *
+import canopen
+import pytest
+import time
+
+
+class TestSDO(object):
+    @classmethod
+    def setup_class(cls):
+        cls.network = create_network()
+        cls.node_id = 19
+        reset_network(cls.network)
+
+        # lss prepare node
+        lss_waiting_state(cls.network)
+        lss_set_node_id(cls.network, cls.node_id)
+        cls.node = canopen.RemoteNode(cls.node_id, 'SCTB_CANopenPressureSensor0xC001.eds')
+        cls.node.associate_network(cls.network)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.node.nmt.state = 'PRE-OPERATIONAL'
+        cls.node.remove_network()
+        cls.network.disconnect()
+
+    def test_clear_tpo_config(self):
+        self.node.tpdo.read()
+        for tpdoN in self.node.tpdo:
+            self.node.tpdo[tpdoN].clear()
+        self.node.tpdo.save()
+
+    def test_simple_tpdo(self):
+        def tpfo_cb(msg):
+            print('%s received' % msg.name)
+            for var in msg:
+                print('-> {} = {}'.format(var.name, var.raw))
+
+        self.node.tpdo.read()
+        tpdo = self.node.tpdo[1]
+        tpdo.add_variable(0x2500, 1)
+        tpdo.add_variable(0x2500, 2)
+        tpdo.trans_type = 254
+        tpdo.event_timer = 1000
+        tpdo.enabled = True
+        self.node.tpdo.save()
+
+        tpdo.add_callback(tpfo_cb)
+        self.node.nmt.state = 'OPERATIONAL'
+        time.sleep(2)
+        self.node.nmt.state = 'PRE-OPERATIONAL'
